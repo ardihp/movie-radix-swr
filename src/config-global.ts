b2cf64aae58ca61bfbd6eec9b3994a2ca0282c0a
ardihp <ardihp8@gmail.com>
2024-01-11 18:08:06 +0700

    feat(config): variable global

--- src/config-global.ts
+++ src/config-global.ts
@@ -1,3 +1,4 @@
 export const HOST_API = process.env.NEXT_PUBLIC_HOST_API;
 export const HOST_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 export const HOST_ACCESS_TOKEN = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN;
+export const HOST_DOMAIN_ASSETS = process.env.NEXT_PUBLIC_HOST_DOMAIN_ASSETS;
