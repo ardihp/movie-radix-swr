c713044e6d528d9a9d8d045949e6a6eda4a42a3a
ardihp <ardihp8@gmail.com>
2024-02-15 18:10:31 +0700

    feat(axios): endpoint get upcoming & top rated & popular movie

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -19,5 +19,8 @@ export default axiosInstance;
 export const endpoints = {
   movie: {
     now_playing: "/movie/now_playing",
+    popular: "/movie/popular",
+    top_rated: "/movie/top_rated",
+    upcoming: "/movie/upcoming",
   },
 };
